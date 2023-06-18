1a52d64d84f530a799ad61b42c0e4e1fd14bf3d2
lucas19fonseca <126473218+lucas19fonseca@users.noreply.github.com>
2023-06-18 19:17:58 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,29 @@
 package q2
 
+import (
+	"fmt"
+	"strings"
+)
+
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+
+	words := strings.Fields(text)
+
+	if len(words) == 0 {
+		return 0, fmt.Errorf("texto vazio")
+	} else if len(strings.TrimSpace(text)) == 0 {
+		return 0, fmt.Errorf("texto vazio")
+	} else if len(text) == 0 {
+		return 0, fmt.Errorf("texto vazio")
+	}
+
+	totalLetter := 0
+
+	for _, word := range words {
+		totalLetter += len(word)
+	}
+
+	average := float64(totalLetter) / float64(len(words))
+
+	return average, nil
 }
